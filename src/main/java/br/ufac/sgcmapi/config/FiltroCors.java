871c08fd48dbe65e745e1c27452efc130aa62ddc
André Luiz <44846083+andrecko@users.noreply.github.com>
2022-06-08 16:16:45 -0500

    Update FiltroCors.java

--- src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
+++ src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
@@ -16,7 +16,7 @@ public class FiltroCors {
 
         CorsConfiguration configCors = new CorsConfiguration();
         configCors.setAllowCredentials(true);
-        configCors.setAllowedOrigins(Arrays.asList("https://sgcmapp.herokuapp.com"));
+        configCors.setAllowedOrigins(Arrays.asList("https://sgcmapi-meu.herokuapp.com"));
         configCors.setAllowedMethods(Arrays.asList("*"));
         configCors.setAllowedHeaders(Arrays.asList("*"));
 
